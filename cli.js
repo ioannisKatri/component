ad76458250a40d0ef0735744c492426b72d750da
iaonnisKatri <ioanniskatri@hotmail.com>
2021-07-09 17:44:00 +0200

    test

--- cli.js
+++ cli.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-const {hooks} = require('./lib/hooks');
-
 const program = new Command()
+
+const {hooks} = require('./lib/hooks');
+const {Command} = require('commander');
 
 program
     .option('-i, --init', "Git hooks")
     .parse(process.argv);
 
 const options = program.opts();
-
 
 if (options.init) {
     hooks();
